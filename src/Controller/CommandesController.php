bef2dceab47137c0767266fb4e8f516232a3966a
Zicrou <aazseck@gmail.com>
2022-12-09 18:22:39 +0100

    Added Commandes MVC and Template

---
+++ src/Controller/CommandesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+class CommandesController extends AppController
+{
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $commandes = $this->Paginator->paginate($this->Commandes->find());    
+        $this->set(compact('commandes'));
+    }
+
+    public function view($slug = null)
+    {
+        $commande = $this->Commandes->findBySlug($slug)->firstOrFail();
+        $this->set(compact('commande'));
+    }
+
+    public function add()
+    {
+        $commande = $this->Commandes->NewEmptyEntity();
+        if ($this->request->is("post")) 
+        {
+            $commande = $this->Commandes->patchEntity($commande, $this->request->getData());
+
+            if ($this->Commandes->save($commande))
+            {
+                $this->Flash->success("Votre commande à été sauvegardé!");
+                return $this->redirect(["action" => "index"]);
+            }
+            $this->Flash->error("Impossible d'ajouter votre commande.");
+        }
+        $this->set(compact("commande"));
+    }
+
+    public function edit($slug)
+    {
+        $commande = $this->Commandes->findBySlug($slug)->firstOrFail();
+
+        if ($this->request->is(["post", "put"])) 
+        {
+            $commande = $this->Commandes->patchEntity($commande, $this->request->getData());
+            
+            if ($this->Commandes->save($commande))
+            {
+                $this->Flash->success("Votre commande à été modifié!");
+                return $this->redirect(["action" => "index"]);
+            }
+            $this->Flash->error("Impossible de modifier votre commande.");
+        }
+        $this->set(compact("commande"));
+    }
+
+    public function delete($slug)
+    {
+        $this->request->allowMethod(["post", "delete"]);
+        
+        $commande = $this->Commandes->findBySlug($slug)->firstOrFail();
+        
+        if ($this->Commandes->delete($commande)) {
+            $this->Flash->success(__('Les données de "{0}" ont été supprimé!', $commande->nom) );
+            return $this->redirect(["action" => "index"]);
+        }
+    }
+}
---
+++ src/Model/Table/CommandesTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\Event\EventInterface;
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
+
+class CommandesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+        
+    }
+
+//    public function beforeSave(EventInterface $event, $entity, $options)
+//    {
+//        if ($entity->isNew() && !$entity->slug) {
+//            $sluggedNom = Text::slug($entity->nom);
+//            $entity->slug = substr($sluggedNom, 0, 191);
+//        }
+//    }
+//
+//    public function validationDefault(Validator $validator): Validator
+//    {
+//        $validator
+//            ->notEmptyString("nom", "Ce champ ne peut etre vide.")
+//            ->minLength("nom", 5, "5 caracteres au minimu!")
+//            ->maxLength("nom", 255)
+//
+//            ->notEmptyString("description")
+//            ->minLength("nom", 10, "10 caracteres au minimu!");
+//
+//            return $validator;
+//    }
+} 
